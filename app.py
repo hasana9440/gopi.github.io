c660cef11c7d192527e72899dbf5249a99ed1e7b
hasana9440 <84851251+hasana9440@users.noreply.github.com>
2023-02-03 08:50:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,6 @@ def index():
             return render_template('index.html',prediction_result="please enter valid input")
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+'''if __name__ == "__main__":
+    app.run(debug=True)'''
+app.run()
